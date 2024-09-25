3bc5ac008816c982cec3e44e5f6997dd5c5e06ce
liamslost <liamslost@googlemail.com>
2024-09-25 15:47:13 +0100

    new malformed rule added

--- Tests/DisplayLocationsTest.php
+++ Tests/DisplayLocationsTest.php
@@ -5,7 +5,7 @@ use PHPUnit\Framework\TestCase;
 
 
 class DisplayLocationsTest extends TestCase{
-    public function testDisplaylocations():void
+    public function testDisplaylocations_success():void
 {
     $locations = [[
         'location'=>'Florida',
@@ -14,24 +14,40 @@ class DisplayLocationsTest extends TestCase{
         'photo' => 'photo'
     ]];
 
-    $expected = "<div class='itemcard'>
-            <div class='imageplaceholder'><img class='imagesizing' src='photo' alt='Tampa'><p class='imagetext'>Tampa,Florida</p></div>
-            <div class='textcard'>
-            <p>Ice Hockey</p>
-            </div>
-            </div>";
+    $expected ="<div class='itemcard'>";
+    $expected .="<div class='imageplaceholder'>";
+    $expected .="<img class='imagesizing' src='photo' alt='Tampa'>";
+    $expected .="<p class='imagetext'>Tampa,Florida</p>";
+    $expected .="</div>";
+    $expected .="<div class='textcard'>";
+    $expected .="<p>Ice Hockey</p>";
+    $expected .="</div>";
+    $expected .="</div>";
+
     $actual = DisplayLocations($locations);
 
     $this->assertEquals($expected,$actual);
 }
-public function testDisplaylocationstype ():void
+public function testDisplaylocationstype_failure ():void
 {
     $this->expectException(TypeError::class);
 
     DisplayLocations('This is not an array');
-
-
 }
 
+public function testDisplaylocationsMalformedtest ():void
+{
+    $locations = [
+        [
+        'location'=>'Florida',
+        'sublocation'=>'Tampa',
+        'attraction'=>'Ice Hockey',
+    ]];
+
+    $this->expectException(InvalidArgumentException::class);
+
+    DisplayLocations($locations);
+
+    }
 
 }
--- src/DisplayLocations.php
+++ src/DisplayLocations.php
@@ -4,14 +4,19 @@ function DisplayLocations(array $allLocations) : string
 {
     $result = '';
     foreach ($allLocations as $item) {
-        $result .=
-            "<div class='itemcard'>
-            <div class='imageplaceholder'><img class='imagesizing' src='{$item['photo']}' alt='{$item['sublocation']}'><p class='imagetext'>{$item['sublocation']},{$item['location']}</p></div>
-            <div class='textcard'>
-            <p>{$item['attraction']}</p>
-            </div>
-            </div>";
+        if (!isset($item['photo']) || !isset($item['sublocation']) || !isset($item['location']) || !isset($item['attraction'])) {
+            throw new InvalidArgumentException("Unassigned key");
+        }
+        $result .="<div class='itemcard'>";
+        $result .="<div class='imageplaceholder'>";
+        $result .="<img class='imagesizing' src='{$item['photo']}' alt='{$item['sublocation']}'>";
+        $result .="<p class='imagetext'>{$item['sublocation']},{$item['location']}</p>";
+        $result .="</div>";
+        $result .="<div class='textcard'>";
+        $result .="<p>{$item['attraction']}</p>";
+        $result .="</div>";
+        $result .="</div>";
 
     }
     return $result;
-};
+}
