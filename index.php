82751ac939746b3294cd48a8c54be99a0e15e9fb
liamslost <liamslost@googlemail.com>
2024-09-27 13:49:27 +0100

    adding photots

--- index.php
+++ index.php
@@ -29,6 +29,7 @@ $All_statesData = DBgetAllDataAll_States($db);
 ?>
 <html lang="eng">
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
 </head>
 <title>Collections App</title>
